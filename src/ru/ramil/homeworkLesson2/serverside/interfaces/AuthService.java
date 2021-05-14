8b61a37ad3308f186d9cf82fb73c7bb22a3ab529
Ramil <ram-kur@mail.ru>
2021-05-15 00:33:35 +0400

    Удалил код чата

--- src/ru/ramil/homeworkLesson2/serverside/interfaces/AuthService.java
+++
@@ -1,9 +0,0 @@
-package ru.ramil.homeworkLesson2.serverside.interfaces;
-
-import java.sql.*;
-
-public interface AuthService {
-    void start();
-    void stop();
-    String getNickByLoginAndPassword(String login, String password) throws SQLException;
-}
